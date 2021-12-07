40b917cf6f297626ace12c9bdda918a84866800b
s-wadnere <shraddha.wadnere@ugamsolutions.com>
2021-12-07 17:12:42 +0530

    Workflow & Custom Workflow Model

---
+++ core/src/main/java/com/ugamsProj/core/models/Impl/UgamProjectWorkflow.java
@@ -0,0 +1,48 @@
+package com.ugamsProj.core.models.Impl;
+
+import com.adobe.granite.workflow.WorkflowException;
+import com.adobe.granite.workflow.WorkflowSession;
+import com.adobe.granite.workflow.exec.WorkItem;
+import com.adobe.granite.workflow.exec.WorkflowData;
+import com.adobe.granite.workflow.exec.WorkflowProcess;
+import com.adobe.granite.workflow.metadata.MetaDataMap;
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Component;
+
+import javax.jcr.*;
+import javax.jcr.lock.LockException;
+import javax.jcr.nodetype.ConstraintViolationException;
+import javax.jcr.version.VersionException;
+
+@Component(
+        service = WorkflowProcess.class,
+        property = {
+                "process.label" + " = ugam project Workflow",
+                Constants.SERVICE_VENDOR + "=ugam project",
+                Constants.SERVICE_DESCRIPTION + " = Custom ugams project workflow step."
+        }
+)
+public class UgamProjectWorkflow implements WorkflowProcess{
+    @Override
+    public void execute(WorkItem workItem, WorkflowSession workflowSession, MetaDataMap metaDataMap) throws WorkflowException {
+        try {
+            WorkflowData workflowData = workItem.getWorkflowData();
+            if (workflowData.getPayloadType().equals("JCR_PATH")) {
+                Session session = workflowSession.adaptTo(Session.class);
+            String path = workflowData.getPayload().toString() + "/jcr:content";
+            Node node = (Node) session.getItem(path);
+            String[] processArgs = metaDataMap.get("PROCESS_ARGS", "string").toString().split(",");
+            for (String wfArgs : processArgs) {
+                String[] args = wfArgs.split(":");
+                String prop = args[0];
+                String value = args[1];
+                if (node != null) {
+                    node.setProperty(prop, value);
+                }
+            }
+        }
+    } catch (Exception e) {
+
+        }
+    }
+}
